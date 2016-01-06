52b2f497afc03223fed4dcc007b8090c976c96ac
Mikkel Gravgaard <mikkel@klokke.dk>
2016-01-07 00:17:42 +0100

    raspberry pi fix

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ var midi = require('midi')
 var f = require('./fun.js');
 
 var o = new midi.output();
-o.openPort(0);
+var i = new midi.input();
+var n = 0;
+i.openPort(n); // This is apparently necessary for sound to come out on RPi!?
+
+console.log("using port", o.getPortName(n));
+o.openPort(n);
 
 q = [];
 
 f.startLoop(10)
 
 var now = perfNow()+100;
+var d = 100;
 [48,48,48,50,52,52,52].map((x,i) => {
-	f.queue(q,{o: o, v: [0x90, x, 64], t: now+i*250})	
-	f.queue(q,{o: o, v: [0x80, x, 64], t: now+i*250+100})	
+	f.queue(q,{o: o, v: [0x90, x, 64], t: now+i*d})	
+	f.queue(q,{o: o, v: [0x80, x, 64], t: now+i*d+d/2})	
 })
 
 
