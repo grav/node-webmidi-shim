e7c854537c11d82879f702c2bad91a5ddffde52e
Mikkel Gravgaard <mikkel@klokke.dk>
2017-02-12 00:30:54 +0100

    reformat

--- webmidi.js
+++ webmidi.js
@@ -2,44 +2,44 @@ const midi = require('midi');
 const perfNow = require("performance-now");
 const cp = require('child_process');
 
-function play(o,v){
+function play(o, v) {
 	o.sendMessage(v);
 }
 
-function queue(q,e){
+function queue(q, e) {
 	var now = perfNow();
-	if(e.t<now){
-		play(e.o,e.v);
+	if (e.t < now) {
+		play(e.o, e.v);
 	} else {
 		q.push(e);
 	}
 }
 
-function playNext(q){
+function playNext(q) {
 	var c = 0;
 	var now = perfNow();
-	for (e of q){
-		if(e.t<now){
+	for (e of q) {
+		if (e.t < now) {
 			c++;
-			play(e.o,e.v);
+			play(e.o, e.v);
 		}
 	}
 	return c > 0 ? q.slice(c) : q;
-	
+
 }
 
-function startLoop(ma,i){
-	return setInterval(_=>{
+function startLoop(ma, i) {
+	return setInterval(_ => {
 		ma._q = playNext(ma._q);
 	}, i);
 }
 
-function createOutput(ma,portNumber) {
+function createOutput(ma, portNumber) {
 	var o = new midi.output();
 	var name = o.getPortName(portNumber);
 	o.openPort(portNumber);
 	return {
-		send: function(v, t) {
+		send: function (v, t) {
 			// todo - handle no t
 			queue(ma._q, {
 				o: o,
@@ -50,7 +50,7 @@ function createOutput(ma,portNumber) {
 		id: name,
 		name: name
 	}
-	
+
 }
 
 function createInput(portnumber) {
@@ -85,26 +85,26 @@ function createInput(portnumber) {
 
 }
 
-function requestMIDIAccess(){
+function requestMIDIAccess() {
 	var ma = {
 		_q: [],
-		sysexEnabled: false,		
+		sysexEnabled: false,
 	};
 	var outputs = [];
 	var nOutputs = new midi.output().getPortCount();
-	for(var portNumber=0; portNumber < nOutputs; portNumber++){
-		var o = createOutput(ma,portNumber);
+	for (var portNumber = 0; portNumber < nOutputs; portNumber++) {
+		var o = createOutput(ma, portNumber);
 		outputs.push(o);
 	}
 
 	var inputs = [];
 	var nInputs = new midi.input().getPortCount();
-	for(var portNumber=0; portNumber < nInputs; portNumber++){
+	for (var portNumber = 0; portNumber < nInputs; portNumber++) {
 		var i = createInput(portNumber);
 		inputs.push(i);
 	}
 	ma.outputs = {
-		keys: _ => outputs.map(o=>o.id)[Symbol.iterator](),
+		keys: _ => outputs.map(o => o.id)[Symbol.iterator](),
 		values: _ => outputs[Symbol.iterator]()
 	};
 	startLoop(ma, 10);
