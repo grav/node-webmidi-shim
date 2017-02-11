08b0d5e6a724fdca02c01c2278d5a202efd72760
Mikkel Gravgaard <mikkel@klokke.dk>
2017-02-12 00:02:47 +0100

    use child process for inputs, since node-midi tends to crash otherwise

--- webmidi.js
+++ webmidi.js
@@ -1,4 +1,6 @@
-var perfNow = require("performance-now");
+const midi = require('midi');
+const perfNow = require("performance-now");
+const cp = require('child_process');
 
 function play(o,v){
 	o.sendMessage(v);
@@ -32,10 +34,10 @@ function startLoop(ma,i){
 	}, i);
 }
 
-function createOutput(midi,idx,ma){
+function createOutput(ma,portNumber) {
 	var o = new midi.output();
-	var name = o.getPortName(idx);
-	o.openPort(idx);
+	var name = o.getPortName(portNumber);
+	o.openPort(portNumber);
 	return {
 		send: function(v, t) {
 			// todo - handle no t
@@ -51,62 +53,68 @@ function createOutput(midi,idx,ma){
 	
 }
 
-function createInput(midi,idx){
-	var i = new midi.input();
-	var name = i.getPortName(idx);
-	try {
-		i.openPort(idx);
-	} catch (e){
-	  	console.log(e)
-	}
-	var input = {
-		_i: i,
-		id: name,
-		name: name,
-		onmidimessage: null
-	};
-	i.on('message', (d,m) => {
-		if(input.onmidimessage){
-			var e = {
-				timestamp: d,
-				data: m
+function createInput(portnumber) {
+	return new Promise((res, rej) => {
+		var input = {
+			id: null,
+			name: null,
+			onmidimessage: null
+		};
+
+		let inProcess = cp.fork(`${__dirname}/input-child.js`, [portnumber]);
+
+		['close', 'disconnect', 'error', 'exit'].forEach((eventType) => {
+			inProcess.on(eventType, () => {
+				console.log("inProcess sent (" + eventType + ")");
+				// todo - respawn input-child
+			});
+		});
+
+		inProcess.on('message', m => {
+			switch (m.method) {
+				case 'portname':
+					input.name = input.id = m.payload;
+					res(input)
+				case 'midiin':
+					if (input.onmidimessage) {
+						input.onmidimessage(m.payload)
+					}
 			}
-			input.onmidimessage(e);
-		}
+		})
 	});
-	
-	return input;
+
 }
 
 function requestMIDIAccess(){
-	var midi = require('midi');
 	var ma = {
 		_q: [],
 		sysexEnabled: false,		
 	};
 	var outputs = [];
 	var nOutputs = new midi.output().getPortCount();
-	for(var idx=0; idx < nOutputs; idx++){
-		var o = createOutput(midi,idx,ma);
+	for(var portNumber=0; portNumber < nOutputs; portNumber++){
+		var o = createOutput(ma,portNumber);
 		outputs.push(o);
 	}
 
 	var inputs = [];
 	var nInputs = new midi.input().getPortCount();
-	for(var idx=0; idx < nInputs; idx++){
-		var i = createInput(midi,idx,ma);
+	for(var portNumber=0; portNumber < nInputs; portNumber++){
+		var i = createInput(portNumber);
 		inputs.push(i);
 	}
 	ma.outputs = {
 		keys: _ => outputs.map(o=>o.id)[Symbol.iterator](),
 		values: _ => outputs[Symbol.iterator]()
 	};
-	ma.inputs = {
-		keys: _ => inputs.map(i=>i.id)[Symbol.iterator](),
-		values: _ => inputs[Symbol.iterator]()
-	};
-	startLoop(ma,10);
-	return Promise.resolve(ma);
+	startLoop(ma, 10);
+	return Promise.all(inputs).then(ins => {
+		ma.inputs = {
+			keys: _ => ins.map(i => i.id)[Symbol.iterator](),
+			values: _ => ins[Symbol.iterator]()
+		};
+		return ma
+	})
 }
 
 module.exports = {
